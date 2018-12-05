a7482a7cbdd399fb3234fbe7c84da5bb95db2417
Unrud <unrud@outlook.com>
2018-12-05 19:49:51 +0100

    bump version to 0.0.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="djpdf",
-    version="0.0.6",
+    version="0.0.7",
     packages=["djpdf", "djpdfgui"],
     package_data={
         "djpdf": ["tesseract-pdf.ttf", "to-unicode.cmap"],
