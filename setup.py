be3447b1d8c8d4a1e387d5c7bbb9d16b4f260c78
Unrud <unrud@outlook.com>
2020-07-24 23:04:58 +0200

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ if enable_gui:
 
 setup(
     name="djpdf",
-    version="0.1.1",
+    version="0.1.2",
     packages=packages,
     package_data=package_data,
     entry_points={"console_scripts": console_scripts},
