d704caebdcefaa34ad947b8e0f1713c6d0588ad4
Unrud <unrud@outlook.com>
2023-12-19 01:20:43 +0100

    Bump version to 0.5.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="djpdf",
-    version="0.5.2",
+    version="0.5.3",
     packages=["djpdf"],
     package_data={"djpdf": ["argyllcms-srgb.icm", "tesseract-pdf.ttf",
                             "to-unicode.cmap"]},
